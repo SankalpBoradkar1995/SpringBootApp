3566d40cbe3fc2fa841493c3440eb5ff7c22f7c6
Sankalp <boradkarsankalp6@gmail.com.com>
2022-02-02 15:13:19 +0530

    Configured Main class with SpringBootServeletInilizer

--- src/main/java/com/vms/root/VehicleManagementSystemApplication.java
+++ src/main/java/com/vms/root/VehicleManagementSystemApplication.java
@@ -2,10 +2,17 @@ package com.vms.root;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 
 @SpringBootApplication
-public class VehicleManagementSystemApplication {
+public class VehicleManagementSystemApplication extends SpringBootServletInitializer {
 
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+         return application.sources(VehicleManagementSystemApplication.class);
+    }
 	public static void main(String[] args) {
 		SpringApplication.run(VehicleManagementSystemApplication.class, args);
 	}
