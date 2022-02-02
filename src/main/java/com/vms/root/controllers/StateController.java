6427dc3cf719442b8922743dcb6907dc66bb8b30
Sankalp <boradkarsankalp6@gmail.com.com>
2022-02-03 04:15:03 +0530

    Updated State controllers and services

--- src/main/java/com/vms/root/controllers/StateController.java
+++ src/main/java/com/vms/root/controllers/StateController.java
@@ -1,13 +1,58 @@
 package com.vms.root.controllers;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.vms.root.models.State;
+import com.vms.root.services.StateService;
 
 @Controller
-public class StateController {
+public class StateController 
+{
+	@Autowired
+	StateService stateRepo;
+	
 	@GetMapping("/state")
-	public String getStates()
+	public String getStates(Model model)
 	{
+		List<State> stateDetails = stateRepo.getAllStates();
+		model.addAllAttributes(stateDetails);
 		return "state";
+	}
+	
+	@RequestMapping(value="/state/update",method= {RequestMethod.GET,RequestMethod.PUT})
+	public String updateState(State state)
+	{
+		stateRepo.saveState(state);
+		return "redirect:/state";
+	}
+	
+	@GetMapping("/state/findById")
+	@ResponseBody
+	public Optional<State> findStateById(Integer id)
+	{
+		return stateRepo.findStateByID(id);
+	}
+	
+	@RequestMapping(value="/state/delete", method=RequestMethod.DELETE)
+	public String deleteState(int id)
+	{
+		stateRepo.deleteState(id);
+		return "redirect:/state";
+	}
+	
+	@RequestMapping(value="/state/addNew", method= {RequestMethod.GET,RequestMethod.POST})
+	public String addState(State state)
+	{
+		stateRepo.saveState(state);
+		return "redirect:/state";
 	}
 }
--- src/main/java/com/vms/root/services/StateService.java
+++ src/main/java/com/vms/root/services/StateService.java
@@ -1,8 +1,43 @@
 package com.vms.root.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-@Service
-public class StateService {
+import com.vms.root.models.State;
+import com.vms.root.repositories.StateRepository;
 
+@Service
+public class StateService 
+{
+	@Autowired
+	StateRepository stateRepo;
+	// Get all states
+	
+	public List<State> getAllStates()
+	{
+		return stateRepo.findAll();
+	}
+	
+	//Update State details
+	
+	public void saveState(State state)
+	{
+		stateRepo.save(state);
+	}
+	
+	//Find state by ID
+	public Optional<State> findStateByID(Integer id)
+	{
+		return stateRepo.findById(id);
+	}
+	
+	//Delete by ID
+	public void deleteState(int id)
+	{
+		stateRepo.deleteById(id);
+	}
+	
 }
