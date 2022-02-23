a6ac9583c953d37a24937e0f2f69fda0567cf01a
Sankalp <boradkarsankalp6@gmail.com.com>
2022-02-24 02:16:41 +0530

    Added assets model, service, repository and controller. Need to map UI with asset URIs

---
+++ src/main/java/com/vms/root/repositories/AssetRepository.java
@@ -0,0 +1,18 @@
+package com.vms.root.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.vms.root.models.Assets;
+
+@Repository
+public interface AssetRepository extends JpaRepository<Assets, Integer>
+{
+	@Query("select a from Assets a where "
+			+ "concat(a.assettype, a.expirydate, a.hardwarepurpose, a.macid, a.licencetype, a.licencedetails, a.softwarepurpose) like %?1%")
+	public List<Assets> getByKeyword(String keyword);
+	
+}
